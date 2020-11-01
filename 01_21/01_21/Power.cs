fca7893d5b9c4442eda00baf62e62d120f5404d3
FranciscoPires <frelvaspires@gmail.com>
2020-11-01 10:59:43 +0000

    01_21 adicionado

---
+++ 01_21/01_21/Power.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace _01_21
+{
+    public class Power
+    {
+        public string Description { get; set; }
+        public int Range { get; set; }
+    }
+}
---
+++ 01_21/01_21/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _01_21
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
