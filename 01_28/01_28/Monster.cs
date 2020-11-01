53ad39717ae889bc1ed0644f580c900dbfd32828
FranciscoPires <frelvaspires@gmail.com>
2020-11-01 11:20:25 +0000

    01_28 adicionado

---
+++ 01_28/01_28/Monster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_28
+{
+    class Monster
+    {
+        public const double MaxHealth = 100;
+        public const int MaxStrength = 200;
+        public MonsterType Type { get; set; }
+        public double Health { get; set; }
+        public int Strength { get; set; }
+    }
+}
