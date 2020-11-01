398b2c16fe7c4e48aba5d2f885dc4be369d80caa
FranciscoPires <frelvaspires@gmail.com>
2020-11-01 14:28:13 +0000

    01_31 adicionado

---
+++ 01_31/01_31/Loot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_31
+{
+    public class Loot
+    {
+        public LootType WhatKindOfLootAmI { get; set; }
+        public string Description { get; set; }
+        public ulong Value { get; set; }
+    }
+}
