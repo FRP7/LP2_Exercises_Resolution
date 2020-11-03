2762689a58b7d517228c2278693aa3ba24dad05b
Francisco Pires <frelvaspires@gmail.com>
2020-11-03 12:34:14 +0000

    retirado código morto do construtor

--- 01_22/01_22/01_22/Program.cs
+++ 01_22/01_22/01_22/Program.cs
@@ -7,14 +7,13 @@ namespace _01_22
         // Variável da classe GameItem
         private static GameItem _Gameitem;
         //
-
+                                                                               
         static void Main(string[] args)
         {
             // Chamar o construtor e definir os valores
-            _Gameitem = new Sword(name: "Sword of king Arthur",
-                description: "One of the most important swords in history",
-                length: 50, typeofmetal: TypeofMetal.steel,
-                condition: WeaponCondition.fresh);
+            _Gameitem = new Sword("Sword of king Arthur",
+                "One of the most important swords in history",
+                50, TypeofMetal.steel, WeaponCondition.fresh);
             //
         }
     }
