3f4ea45cf51b4ffcd07e9ead87cc4a5be5222c3e
FranciscoPires <frelvaspires@gmail.com>
2020-11-04 11:32:15 +0000

    revisão

--- 01_22/01_22/01_22/Program.cs
+++ 01_22/01_22/01_22/Program.cs
@@ -5,13 +5,13 @@ namespace _01_22
     class Program
     {
         // Variável da classe GameItem
-        private static GameItem _Gameitem;
+        //private static GameItem _Gameitem;
         //
                                                                                
         static void Main(string[] args)
         {
             // Chamar o construtor e definir os valores
-            _Gameitem = new Sword("Sword of king Arthur",
+            GameItem gameitem = new Sword("Sword of king Arthur",
                 "One of the most important swords in history",
                 50, TypeofMetal.steel, WeaponCondition.fresh);
             //
