57bc6d801c1e731fa67684811e7b9b10bb5eebeb
FranciscoPires <frelvaspires@gmail.com>
2020-11-04 16:21:06 +0000

    solução no visual studio criada

---
+++ 01_41/01_41/HighScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_41
+{
+    class HighScoreManager
+    {
+        private List<Tuple<string, float>> scorelist =
+            new List<Tuple<string, float>>(10);
+
+        public HighScoreManager(string name) {
+
+        }
+
+        private void AddScore(string name, float score) {
+
+        }
+
+        public override string ToString() {
+            return base.ToString();
+        }
+
+        private void GetScores() {
+
+        }
+    }
+}
---
+++ 01_41/01_41/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _01_41
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
