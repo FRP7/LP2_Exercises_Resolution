6970f68ff08839a183e9213311d6dbb1e61ab985
FranciscoPires <frelvaspires@gmail.com>
2020-11-04 18:31:18 +0000

    ja consegue inserir valores

--- 01_41/01_41/HighScoreManager.cs
+++ 01_41/01_41/HighScoreManager.cs
@@ -2,46 +2,56 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Linq;
 
 namespace _01_41
 {
     class HighScoreManager
     {
         private List<Tuple<string, float>> scorelist =
-            new List<Tuple<string, float>>(10);
+            new List<Tuple<string, float>>();
 
         public HighScoreManager(string name) {
             if (File.Exists(name)) {
                 Console.WriteLine("Ler ficheiro");
-                Open();
+                Read();
             }
             else if(!File.Exists(name)) {
-                Console.WriteLine("Não existe, a coleção vai ser inicializada sem elementos");
+                Console.WriteLine("Ficheiro não existe.");
+                // Inicializar coleção sem elementos
             } 
             else if (Path.GetExtension(name) != ".txt") {
                 throw new InvalidOperationException();
             }
         }
 
-        private void Open() {
+        private void Read() {
             StreamReader streamreader = new StreamReader("highscores.txt");
-            string teste;
-            while((teste = streamreader.ReadLine()) != null) {
-                Console.WriteLine(teste);
-            }
+            // Meter o conteúdo do ficheiro na coleção
             streamreader.Close();
         }
 
-        private void AddScore(string name, float score) {
+        public void AddScore(string name, float score) {
+                scorelist.Add(new Tuple<string, float>(name, score));
+                Console.WriteLine("Count: " + scorelist.Count);
+            /*while(scorelist.Count > 10) {
+                // remover o que está no fundo da lista (tenho que ordenar primeiro)
+            }*/
+        }
 
+        private void Save() {
+            // guardar os scores no ficheiro
         }
 
         public override string ToString() {
+            // devolver uma string contendo uma tabela formatada com nomes 
+            //e scores do mais alto ao mais baixo
             return base.ToString();
         }
 
         private void GetScores() {
-
+            // método iterável que retorna de forma ordenada (mais alto até mais baixo)
+            // todos tuplos guardados na coleção
         }
     }
 }
--- 01_41/01_41/Program.cs
+++ 01_41/01_41/Program.cs
@@ -4,9 +4,40 @@ namespace _01_41
 {
     class Program
     {
+        private static HighScoreManager highscoremanager;
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-            HighScoreManager highscoremanager = new HighScoreManager("highscores.txt");
+            int inputnumber = 0;
+            string userinput;
+            bool stopinputnumber = false;
+            string inputname;
+            int inputscore;
+            highscoremanager = new HighScoreManager("highscores.txt");
+            //highscoremanager.AddScore();
+            /*while(true) {
+                highscoremanager.AddScore(Console.ReadLine(), 
+                    float.Parse(Console.ReadLine()));
+            }*/
+            while(stopinputnumber == false) {
+                Console.WriteLine("Quantos scores quer colocar?");
+                userinput = Console.ReadLine();
+                if(int.TryParse(userinput, out inputnumber)) {
+                    Console.WriteLine("Pode começar a colocar os scores.");
+                    stopinputnumber = true;
+                } else {
+                    Console.WriteLine("Input incorrecto, tente de novo.");
+                }
+            }
+
+            for (int i = 0; i < inputnumber; i++) {
+                Console.WriteLine("Escreva o nome");
+                userinput = Console.ReadLine();
+                inputname = userinput;
+                Console.WriteLine("Escreva o score");
+                userinput = Console.ReadLine();
+                inputscore = Convert.ToInt32(userinput);
+                highscoremanager.AddScore(inputname, inputscore);
+            }
+
         }
     }
 }
