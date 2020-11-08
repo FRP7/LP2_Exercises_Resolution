53550ffc8aec3820c075071435e9a9b9701a5fe2
FranciscoPires <frelvaspires@gmail.com>
2020-11-08 12:21:00 +0000

    subssquad

--- 02_02/02_02/Program.cs
+++ 02_02/02_02/Program.cs
@@ -8,14 +8,23 @@ namespace _02_02
             //testar as cenas, delete later
             Console.WriteLine("Teste: ");
             Squad squad = new Squad();
+            SubSquad subsquad = new SubSquad();
             Unit unit1 = new Unit("Space Marine", new Vector2(1, 2), 100);
             squad.squadstack.Push(unit1);
-            Unit unit2 = new Unit("Ripper", new Vector2(3, 4), 200);
+            subsquad.subsquadstack.Push(unit1);
+            Unit unit2 = new Unit("Space Marine", new Vector2(3, 4), 100);
             squad.squadstack.Push(unit2);
-            Unit unit3 = new Unit("Healer", new Vector2(5, 6), 50);
+            subsquad.subsquadstack.Push(unit2);
+            Unit unit3 = new Unit("Ripper", new Vector2(3, 4), 200);
             squad.squadstack.Push(unit3);
-            Console.WriteLine("Estado da stack: ");
+            Unit unit4 = new Unit("Healer", new Vector2(5, 6), 50);
+            squad.squadstack.Push(unit4);
+            Console.WriteLine("Estado da stack da squad: ");
             foreach(object item in squad.squadstack) {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("Estado da stack da subsquad: ");
+            foreach (object item in subsquad.subsquadstack) {
                 Console.WriteLine(item.ToString());
             }
         }
