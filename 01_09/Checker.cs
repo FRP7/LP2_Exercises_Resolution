927ce71df6da8ec1b5298345bca0b64562f290aa
FranciscoPires <frelvaspires@gmail.com>
2020-11-14 18:54:19 +0000

    melhorias no código

--- 01_09/Checker.cs
+++ 01_09/Checker.cs
@@ -1,114 +1,122 @@
 ﻿using System;
 
-namespace _01_09
+namespace Checker
 {
     class Checker
     {
         public Checker(int[,] val, string ui) {
+            // Chamar o método Check()
+            Check(val, Convert.ToInt32(ui));
+            //
         }
         private static void Main(string[] args) {
-            // Input do user
-            string userInput;
-
+             // Input do user
+            string userinput;
+            //
             // Número de linhas da array
-            int arrayLines = 0;
-
+            int arraylines = 0;
+            //
             // Número de colunas da array
-            int arrayColumns = 0;
-
+            int arraycolumns = 0;
+            //
             // Array final
             int[,] values;
-
+            //
             // Nova instância de Checker
             Checker _check;
-
+            //
             // Verificar se o número de linhas da matriz está definido
-            bool isMatrixLine = false;
-
+            bool ismatrixline = false;
+            //
             // Verificar se o número de colunas da matriz está definido
-            bool isMatrixColumn = false;
-
+            bool ismatrixcolumn = false;
+            //
             // Verificar se os elementos estão preenchidos
-            bool isValue = false;
-
+            bool isvalue = false;
+            //
             // O valor do input do user
             int value = 0;
+            //
 
             // Definir o número de linhas
-            while (isMatrixLine == false) {
+            while (ismatrixline == false) {
                 Console.WriteLine("Qual é o número de linhas da matriz?");
                 // User Input
-                userInput = Console.ReadLine();
-
+                userinput = Console.ReadLine();
+                //
                 // Tentar convertar o input para int
-                if (Int32.TryParse(userInput, out arrayLines)) {
-                    isMatrixLine = true;
+                if (Int32.TryParse(userinput, out arraylines)) {
+                    ismatrixline = true;
                 } else {
                     Console.WriteLine("Valor inválido, tente de novo.");
                 }
-
+                //
             }
+            //
 
             // Definir o número de linhas
-            while (isMatrixColumn == false) {
+            while (ismatrixcolumn == false) {
                 Console.WriteLine("Qual é o número de colunas da matriz?");
                 // User Input
-                userInput = Console.ReadLine();
-
+                userinput = Console.ReadLine();
+                //
                 // Tentar convertar o input para int
-                if (Int32.TryParse(userInput, out arrayColumns)) {
-                    isMatrixColumn = true;
+                if (Int32.TryParse(userinput, out arraycolumns)) {
+                    ismatrixcolumn = true;
                 } else {
                     Console.WriteLine("Valor inválido, tente de novo.");
                 }
-
+                //
             }
+            //
 
             // Definir o tamanho da array values
-            values = new int[arrayLines, arrayColumns];
+            values = new int[arraylines, arraycolumns];
+            //
 
-            Console.WriteLine($"Tem {arrayLines * arrayColumns} slots");
+            Console.WriteLine($"Tem {arraylines * arraycolumns} slots");
             // Pedir os valores e colocar na array values
-            while (isValue == false) {
-                for (int x = 0; x < arrayLines; x++) {
-                    for (int y = 0; y < arrayColumns; y++) {
+            while (isvalue == false) {
+                for (int x = 0; x < arraylines; x++) {
+                    for (int y = 0; y < arraycolumns; y++) {
                         while (value == 0) {
                             Console.WriteLine($"Insere um valor na" +
                                 $" linha {x + 1} na coluna {y + 1}");
                             // Input do user
-                            userInput = Console.ReadLine();
-
+                            userinput = Console.ReadLine();
+                            //
                             /* Tentar converter o input para float
                              * e colocar na array */
-                            if (Int32.TryParse(userInput, out value)) {
+                            if (Int32.TryParse(userinput, out value)) {
                                 values[x, y] = value;
                                 PrintArray(values);
-
-                            } else {
+                                //
+                            }
+                            else {
                                 Console.WriteLine
                                     ("Valor inválido, tente de novo");
                             }
                         }
                         value = default;
                     }
-                    isValue = true;
+                    isvalue = true;
                 }
             }
-
+            //
             Console.WriteLine("Qual é o valor que quer procurar?");
             // Input do user
-            userInput = Console.ReadLine();
-
+            userinput = Console.ReadLine();
+            //
 
             // Verificar se o método Check() retoma true
-            _check = new Checker(values, userInput);
-
+            _check = new Checker(values, userinput);
+            //
 
             // Mostrar a array
             PrintArray(values);
+            //
 
-
-            if (_check.Check(values, Convert.ToInt32(userInput)) == true) {
+            if (_check.Check(values, Convert.ToInt32(userinput)) == true) {
                 Console.WriteLine("Existe linha");
             } else {
                 Console.WriteLine("Não existe linha");
@@ -119,80 +127,85 @@ namespace _01_09
          * para procurar.
          * Retoma true se encontrar.
          */
-        private bool Check(int[,] x, int ui) {
+        private bool Check(int[,] x , int ui) {
             // Contar a quantidade de valores seguidos na array x
             int sum = 0;
-
+            //
             // Verificar se tem quatro valores seguidos
             bool isfour = false;
-
+            //
 
             // Verificar linhas horizontais
             for (int n = 0; n < x.GetLength(0); n++) {
                 // Retoma a zero quando começa
                 sum = default;
-
+                //
                 for (int j = 0; j < x.GetLength(1); j++) {
                     /* Verificar se existe um valor igual ao que
                      * o user escolheu */
                     if (x[n, j] == ui) {
                         // Soma
                         sum++;
+                        //
                     }
                 }
-
+                //
                 // Verificar se existe quatro números seguidos
                 if (sum >= 4) {
                     isfour = true;
                 }
+                //
             }
-
+            //
             sum = default;
-
             /// Verificar linhas verticais 
             for (int j = 0; j < x.GetLength(1); j++) {
                 // Retoma a zero quando começa
                 sum = default;
-
+                //
                 for (int n = 0; n < x.GetLength(0); n++) {
                     /* Verificar se existe um valor igual ao que o 
                      * user escolheu */
                     if (x[n, j] == ui) {
                         // Soma
                         sum++;
+                        //
                     }
+                    //
                 }
                 // Verificar se existe quatro números seguidos
                 if (sum >= 4) {
                     isfour = true;
                 }
+                //
             }
-
+            ///
             sum = default;
-
             /* Verificar linhas diagonais cima para baixo, 
              * esquerda para direita */
             for (int n = 0; n < 1; n++) {
-                for (int z = 0; z < x.GetLength(0); z++) {
+                for(int z = 0; z < x.GetLength(0); z++) {
                     for (int j = 0; j < x.GetLength(1); j++) {
                         /* Verificar se existe um valor igual ao que o
                          * user escolheu */
                         if (x[z, j] == ui) {
                             // Soma
                             sum++;
+                            //
                         }
-
+                        //
                         // Verificar se existe quatro números seguidos
                         if (sum >= 4) {
                             isfour = true;
                         }
+                        //
                     }
                 }
             }
-
+            //
             // Retomar a soma a zero
             sum = default;
-
+            //
             /* Verificar linhas diagonais cima para baixo, direita para 
             esquerda*/
             for (int n = 0; n < 1; n++) {
@@ -203,44 +216,52 @@ namespace _01_09
                         if (x[z, j] == ui) {
                             sum++;
                         }
-
+                        //
                         // Verificar se existe quatro números seguidos
                         if (sum >= 4) {
                             isfour = true;
                         }
+                        //
                     }
                 }
             }
-
+            //
             // Verificar se foi encontrado quatro valores seguidos 
             if (isfour == true) {
                 return true;
+
             } else {
+
                 return false;
             }
+            //
         }
         // Método para mostrar a array
         private static void PrintArray(int[,] val) {
             // Tamanho da linha
             int rowLength = val.GetLength(0);
-
+            //
             // Tamanho da coluna
             int colLength = val.GetLength(1);
-
+            //
             // Mostrar a array:
             string arrayString = "";
-
+            //
             for (int i = 0; i < rowLength; i++) {
                 for (int j = 0; j < colLength; j++) {
                     // Colocar os valores da array
                     arrayString += string.Format("{0} ", val[i, j]);
+                    //
                 }
                 // Colocar nova linha
                 arrayString += System.Environment.NewLine +
                     System.Environment.NewLine;
+                //
             }
             // Mostrar a array
             Console.WriteLine(arrayString);
+            //
         }
+        //
     }
 }
