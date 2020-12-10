02ebdfd09518615e68d46edb633adcefe00d875c
FranciscoPires <frelvaspires@gmail.com>
2020-12-10 15:55:51 +0000

    Comentários adicionados.

--- 02_02/02_02/Program.cs
+++ 02_02/02_02/Program.cs
@@ -10,19 +10,21 @@ namespace _02_02
     {
         static void Main(string[] args) {
 
+            // Coleção de soldados que quero formar batalhão.
             ICollection<IUnit> units = new List<IUnit>();
 
+            // Criar soldados.
             IUnit unit1 = new Unit("Soldier", new Vector2(5, 10), 100);
             IUnit unit2 = new Unit("Tank", new Vector2(15, 20), 200);
             IUnit unit3 = new Unit("Healer", new Vector2(25, 30), 300);
 
+            // Adicionar os soldados à coleção.
             units.Add(unit1);
             units.Add(unit2);
             units.Add(unit3);
 
+            // Construir o batalhão.
             Squad squad = new Squad(units);
-
-            squad.Teste();
         }
     }
     //
--- 02_02/02_02/Squad.cs
+++ 02_02/02_02/Squad.cs
@@ -3,53 +3,53 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-namespace _02_02 
+namespace _02_02
 {
     /// <summary>
     /// Classe da Squad.
     /// </summary>
     public class Squad : IUnit
     {
-
+        // Coleção dos soldados que fazem parte do batalhão.
         private ICollection<IUnit> squadUnits;
 
+        // Descrição do batalhão.
         public string UnitName { get => $"Group of {squadUnits.Count} units."; }
-        //public Vector2 Position { get; }
+
+        // Posição do batalhão.
         public Vector2 Position {
             get {
                 float x = 0;
                 float y = 0;
-                foreach(IUnit item in squadUnits) {
+                foreach (IUnit item in squadUnits) {
                     x += item.Position.X;
                     y += item.Position.Y;
                 }
                 return new Vector2(x / squadUnits.Count, y / squadUnits.Count);
             }
         }
+
+        // Vida do batalhão.
         public float Health {
             get {
                 float health = 0;
-                foreach(IUnit item in squadUnits) {
-                    health += item.Health; 
+                foreach (IUnit item in squadUnits) {
+                    health += item.Health;
                 }
                 return health / squadUnits.Count;
             }
         }
 
+        // Movimento do batalhão.
         public void Move(Vector2 newPosition) {
-            newPosition = new Vector2(newPosition.X - Position.X, 
+            newPosition = new Vector2(newPosition.X - Position.X,
                 newPosition.Y - Position.Y);
         }
 
+        // Construtor do batalhão.
         public Squad(ICollection<IUnit> units) {
             //squadUnits = new List<IUnit>();
             squadUnits = units;
-        }
-
-        public void Teste() {
-            Console.WriteLine(UnitName);
-            Console.WriteLine($"X: {Position.X}. Y: {Position.Y}");
-            Console.WriteLine("Health: " + Health);
         }
     }
 }
